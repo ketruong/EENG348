5d98ae8d6523236db411c6ae03f887c968e81be8
robb17 <35757591+robb17@users.noreply.github.com>
2018-02-21 22:25:15 -0500

    final

--- Lab2/sketch_part1/sketch_part1.ino
+++ Lab2/sketch_part1/sketch_part1.ino
@@ -1,45 +1,49 @@
-long lastDebounceTime = 0; // time of last debounce  
-long debounceDelay = 50;   // time for button to settle
+long t1 = 0; // time of last debounce  
+long delayT = 50;   // time for button to settle
 int buttonState = 1;       // records current state
 
 void setup(){
-  DDRD = B0000000;
+  DDRD = 0b11111011; // set third pin as output
+  PORTD = 0b00000100; // turn internal pullup resistor on for third pin
   Serial.begin(9600);
 }
 
 void loop(){
   // read portD registers to see if button is pressed
   byte pinD = PIND;
-  byte mask = 0x4;
+  byte mask = 0x4; 
 
   //check if button has settled 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    
+  if ((millis() - t1) > delayT) {
+
+    //for (mask = 0x80; mask >= 0x1; mask = mask >> 0x1) {
+      //Serial.print(PIND & mask);
+    //}
+    //Serial.print("  ");
+    //Serial.print("\n");
+
+    //mask = 0x04;
+
     // check from off to on
-    if ((mask & pinD) != 0 & buttonState == 0) {
-        Serial.println("Button Released!");
-       // change state to released
-        buttonState = 1;
-       
-     // check from on to off
-    } else if ((mask & pinD) == 0 & buttonState != 0) {
+    if ((mask & pinD) == 0 & buttonState != 0) {
         Serial.println("Button Pressed!");
        // change state to pressed
         buttonState = 0;
+       
+     // check from on to off
+    } else if ((mask & pinD) != 0 & buttonState == 0) {
+        Serial.println("Button Released!");
+       // change state to released
+        buttonState = 1;
     }
 
     // update time
-    lastDebounceTime = millis();
+    t1 = millis();
   }
 }
 
 //debugger function to print bytes
 void printBits(byte myByte){
-  byte mask = 0x4;
-  if (mask  & myByte) {
-      Serial.print('1');
-  } else {
-      Serial.print('0');
-  }
+  Serial.print(myByte);
   Serial.print('\n');
 }
